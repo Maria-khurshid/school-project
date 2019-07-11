e18c4fbfd8019120952de3045dfd01b4a351be8e
Maria-khurshid <48955107+Maria-khurshid@users.noreply.github.com>
2019-07-11 12:29:23 +0500

    A group project

---
+++ script.js
@@ -0,0 +1,150 @@
+function disply() {
+    let hidSection = document.querySelector('main');
+    hidSection.classList.remove('hide');
+    hidSection.classList.add('show');
+}
+let school = {
+    name: "The Future",
+    shortDescription: "An English Medium School",
+    longDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore assumenda et accusamus doloribus minima rerum quibusdam distinctio ipsam error ratione autem culpa possimus dolores harum, voluptates dignissimos voluptatem dicta sed.",
+    studentsData: [{
+            name: "Ali",
+            age: 10,
+            group: "A",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Aliaa",
+            age: 12,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Alizay",
+            age: 16,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Binash",
+            age: 16,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Fariha",
+            age: 10,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Gulshon",
+            age: 18,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Hatim",
+            age: 10,
+            group: "A",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Ikram",
+            age: 15,
+            group: "A",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Junaid",
+            age: 12,
+            group: "A",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Khalid",
+            age: 14,
+            group: "A",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Lubnaa",
+            age: 14,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Zahida",
+            age: 17,
+            group: "A",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        }
+    ],
+    fees: 1500,
+    duration: 6,
+    TeachingStaff: [{
+            name: "Abdullah",
+            age: 35,
+            position: "principle",
+            gender: "Male",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Alaina",
+            age: 20,
+            position: "English Teacher",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Bushra",
+            age: 22,
+            position: "Math Teacher",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Samina",
+            age: 21,
+            position: "Techer",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+        {
+            name: "Hiraa",
+            age: 23,
+            position: "Science Teacher",
+            gender: "Female",
+            phoneNumbers: ["03334567896", "+92-457899043"],
+            address: "asdffgfhgjkllllllllhjhgghfd"
+        },
+    ]
+};
